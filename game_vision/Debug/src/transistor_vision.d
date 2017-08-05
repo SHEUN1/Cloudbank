8a36db3d153171a18b4de0b641699abb2b23196f
SHEUN1 <oluadebari@gmail.com>
2017-08-05 11:35:32 +0100

    deleiting game project folder

--- game_vision/Debug/src/transistor_vision.d
+++
@@ -1,8 +0,0 @@
-src/transistor_vision.o: ../src/transistor_vision.cpp ../src/yingyang.h \
- ../src/SeperateObjects.h ../src/featureextraction.h
-
-../src/yingyang.h:
-
-../src/SeperateObjects.h:
-
-../src/featureextraction.h:
